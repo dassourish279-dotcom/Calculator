df9561efc2ae666cbe516881a0204f60fda3b154
Sourish Das <dassourish279@gmail.com>
2026-04-06 02:22:54 +0530

    V0 Readme updated and Code file uploaded

---
+++ V0/Code.java
@@ -0,0 +1,38 @@
+/** V0 */
+import java.util.Scanner; //importing util
+class Code
+{
+    public static void main(String[]args)
+    {
+        double a,b,s=0,t=0;
+        Scanner sc = new Scanner(System.in); //Scanner object
+
+        System.out.print("Enter two nums: ");
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+        s = a+b; //Addition
+        System.out.println("Sum: "+ s);
+
+        System.out.print("Enter two nums: ");
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+        s = a-b; //Substraction
+        System.out.println("Difference: "+ s);
+
+        System.out.print("Enter two nums: ");
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+        s = a*b; //Multiplication
+        System.out.println("Product: "+ s);
+
+        System.out.print("Enter two nums: ");
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+        s = a/b; //Divison -- Quotient
+        t = a%b; //Divison -- Remainder
+        System.out.println("Quotient: "+ s);
+        System.out.println("Remainder: "+ t);
+        
+        sc.close(); //Scanner closing
+    }
+}
--- V0/README.md
+++ V0/README.md
@@ -1,17 +1,20 @@
-<br>V0</br>
+#V0
 
-<br>Description</br>
-<br>This is the first version </br>
+#Description
+This is the first version 
 
-<br>Features</br>
-<br>Input of only 2 nums</br>
-<br>Performs:</br>
-<br>Addtion</br>
-<br>Substraction</br>
-<br>Multiplication</br>
-<br>Division</br>
+#Features#
+1. Performs:
+2. Input of only 2 nums</br> 
+3. Addtion
+4. Substraction
+5. Multiplication
+6. Division
 
-<br>Limitations</br>
-<br>Run only once</br>
-<br>No user choice operations</br>
-<br>Only two inputs</br>
+#Limitations
+1. Run only once
+2. No user choice operations
+3. Only two inputs
+4. Only basic arithmetics
+5. No BODMAS following
+6. No division by 0 error handling
