58db87d47a46c049a9dc64890fe7d511fac64461
Sourish Das <dassourish279@gmail.com>
2026-05-18 16:42:51 +0530

    Started V2

--- Code_V2.java
+++ Code_V2.java
@@ -1,31 +1,51 @@
 import java.util.Scanner;
 class Code_V2
 {
-    double s=0.0,n;
-    public void input()
+    double s;
+    public double sum(double a, double b)
     {
+        s = a+b;
+        return s;
+    }
+    public double sub(double a, double b)
+    {
+        s = a-b;
+        return s;         
+    }
+    public double mult(double a, double b)
+    {
+        s = a*b;
+        return s;
+    }
+    public double divq(double a, double b)
+    {
+        s = a/b;
+        return s;
+    }
+     public double divr(double a, double b)
+    {
+        s = a%b;
+        return s;
+    }
+    public static void main(String[]args)
+    {
+        double n,s=0.0;
+        char ch;
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a num: ");
-        n = sc.nextDouble();
-    }
-    public double sum()
-    {
+        n = sc.nextInt();
         s+=n;
-        return s;
-    }
-    public double sutract()
-    {
-        s-=n;
-        return s;
-    }
-    public double multiply()
-    {
-        s*=n;
-        return s;
-    }
-    public double div()
-    {
-        s/=n;
-        return s;
+        for(int i=2;i>1;i++)
+        {
+            System.out.print("Enter the next num: ");
+            n = sc.nextDouble();
+            System.out.print("Enter your choice (+,-,*,/*,%,^): ");
+            ch = sc.next().charAt(0);
+            switch(ch)
+            {
+                case '+':
+
+            }
+        }
     }
 }
